27153df8f5f8521b4a71d63dbfb52948a3921d16
jonas <125613982+jonasalameda@users.noreply.github.com>
2026-03-03 23:45:40 -0500

    chore: start mail service on Customer Controller

--- app/Controllers/CustomerController.php
+++ app/Controllers/CustomerController.php
@@ -9,6 +9,54 @@ use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+// saving for later, when we have more time
+// try {
+//     //Server settings
+//     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+//     $mail->isSMTP();                                            //Send using SMTP
+//     $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
+//     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+//     $mail->Username   = 'user@example.com';                     //SMTP username
+//     $mail->Password   = 'secret';                               //SMTP password
+//     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+//     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+//     //Recipients
+//     $mail->setFrom('from@example.com', 'Mailer');
+//     $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+//     $mail->addAddress('ellen@example.com');               //Name is optional
+//     $mail->addReplyTo('info@example.com', 'Information');
+//     $mail->addCC('cc@example.com');
+//     $mail->addBCC('bcc@example.com');
+
+//     //Attachments
+//     $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+//     $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+//     //Content
+//     $mail->isHTML(true);                                  //Set email format to HTML
+//     $mail->Subject = 'Here is the subject';
+//     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+//     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+//     $mail->send();
+//     echo 'Message has been sent';
+// } catch (Exception $e) {
+//     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+// }
+
 class CustomerController extends BaseController
 {
     //NOTE: Passing the entire container violates the Dependency Inversion Principle and creates a service locator anti-pattern.
@@ -63,7 +111,7 @@ class CustomerController extends BaseController
                     'error' => "A customer with this email already exists.",
                     'customers' => $customers
                 ];
-                return $this->render($request, $response, 'customerFormView.php', $data);
+                return $this->render($response, 'customerFormView.php', $data);
             }
         }
 
@@ -106,10 +154,21 @@ class CustomerController extends BaseController
                 'customers' => $customers,
                 'success' => 'Customer added successfully!',
             ];
+
+            $message_body = sprintf(
+                "Name: %s %s%nEmail: %s%nPhone: %d%nAddress: %s",
+                $customer_data["first_name"],
+                $customer_data["last_name"],
+                $customer_data["email"],
+                $customer_data["phone"],
+                $customer_data["address"]
+            );
+
+            mail($customer_data["email"], "A customer was created", $message_body);
+
             return $this->render($response, 'customerFormView.php', $data);
         }
-
-        return $this->redirect($request, $response, 'customers.index', $data);
+        // return $this->redirect($request, $response, 'customers.index', $data);
     }
 
     public function error(Request $request, Response $response, array $args): Response
