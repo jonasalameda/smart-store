7b9849eb99d4053fb50202d6d282588f50da7912
markololo <markololo2468@gmail.com>
2026-03-26 09:25:58 +0400

    front view fix yeyyy

--- app/Views/Dashboard.php
+++ app/Views/Dashboard.php
@@ -6,8 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Fridge Dashboard</title>
 
-  <link rel="stylesheet" href="<?= defined('APP_BASE_URL') ? APP_BASE_URL : '' ?>/public/assets/css/layout/sidebar.css"> 
-  <link rel="stylesheet" href="<?= defined('APP_BASE_URL') ? APP_BASE_URL : '' ?>/public/assets/css/dashboard.css">
+<link rel="stylesheet" href="/assets/css/layout/sidebar.css"> 
+<link rel="stylesheet" href="/assets/css/dashboard.css">
     <?php 
     $fridge_data = $data['fridge_data'] ?? [
       'Frig1' => ['temperature' => 0, 'humidity' => 0],
@@ -116,7 +116,7 @@
         <h2>Cooling Fan</h2>
         <div class="fan-row">
 
-          <img id="fan-img" src="<?= defined('APP_BASE_URL') ? APP_BASE_URL : '' ?>/public/assets/images/fan.png" alt="Fan">
+<img id="fan-img" src="/assets/images/fan.png" alt="Fan">
           <!-- commmented this out as well ^^ code above doesnt work on my laptop -->
           <!-- <img id="fan-img" src="assets/images/fan.png" alt="Fan"> -->
           <button id="fan-toggle" class="fan-off">OFF</button>
@@ -134,8 +134,7 @@
   </button>
 </a>
   </main>
-
-  <script src="assets/js/fan.js"></script>
-  <script src="assets/js/dashboard.js"></script>
+<script src="/assets/js/fan.js"></script>
+<script src="/assets/js/dashboard.js"></script>
 </body>
 </html>
