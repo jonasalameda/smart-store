f94612e53ba10eb03d7820ecc59201d54c79c5f4
markololo <markololo2468@gmail.com>
2026-03-26 05:19:59 +0400

    reading from 2 dht11 sensors and publishing, next is connecting to front-en

--- app/Domain/Models/HardwareModel.php
+++ app/Domain/Models/HardwareModel.php
@@ -14,18 +14,28 @@ class HardwareModel extends BaseModel
 
     /**
      * This method is to read the temperature and humidity data from the DHT11 sensor using a Python script, and then publish the data to a specified MQTT topic using the MqttService.
-     * @param topic the MQTT topic to publish the data to
      */
-    public function mqttReadAndPublish(string $topic): void
+    public function mqttReadAndPublish(): void
     {
         // $output = shell_exec("python3 " . APP_BASE_DIR_PATH . "/public/assets/python/TemperatureHumidityReader.py") ?? '{"temperature":0,"humidity":0}';
-        $output = shell_exec("./" . APP_BASE_DIR_PATH . "/public/assets/arduino/TemperatureHumidityReader.py") ?? '{"temperature":0,"humidity":0}';
+        // $output = shell_exec("./" . APP_BASE_DIR_PATH . "/public/assets/arduino/TemperatureHumidityReader.py") ?? '{"temperature":0,"humidity":0}';
+
+        $output = shell_exec("python3 " . APP_BASE_DIR_PATH . "/public/assets/python/read_serial.py") ?? '{"Frig1":{"temperature":0,"humidity":0},"Frig2":{"temperature":0,"humidity":0}}';
+
+        $defaultData = [
+            'Frig1' => ['temperature' => null, 'humidity' => null],
+            'Frig2' => ['temperature' => null, 'humidity' => null]
+        ];
+
+        $data = json_decode($output, true) ?? $defaultData;
 
         // Decode the JSON output from the Python script into an associative array for easier access. I used json format in the Python to make sure it works.
         // $data = json_decode($output, true) ?? ['temperature' => null, 'humidity' => null];
-        $data = json_decode($output, true) ?? ['temperature' => null, 'humidity' => null];
+        // $data = json_decode($output, true) ?? ['temperature' => null, 'humidity' => null];
         
-        $this->mqtt_service->publish($topic, json_encode($data));
+        // $this->mqtt_service->publish($topic, json_encode($data));
+        $this->mqtt_service->publish('Frig1', json_encode($data['Frig1']));
+        $this->mqtt_service->publish('Frig2', json_encode($data['Frig2']));
     }
 }
 
--- public/assets/python/read_serial.py
+++ public/assets/python/read_serial.py
@@ -1,4 +1,4 @@
-# This code is just to read from the ESP32 arduino code
+# This code is just to read from the ESP32 arduino output
 
 import serial
 
@@ -6,4 +6,7 @@ ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=2)
 line = ser.readline().decode('utf-8').strip()
 print(line)
 ser.close()
+
 # https://forum.arduino.cc/t/using-python-to-read-and-process-serial-data-from-arduino/1059079
+
+# do: ls /dev/tty* to find the correct port for the ESP32, it may be different than /dev/ttyUSB0
