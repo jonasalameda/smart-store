17902bc87b21573e3e6f7227aa08ef4fb1760699
Markololo <markololo2468@gmail.com>
2026-04-07 21:43:25 -0400

    made products model for db interaction, next is controllers

--- app/Domain/Models/ProductsModel.php
+++ app/Domain/Models/ProductsModel.php
@@ -1,1 +1,125 @@
+<?php
 
+namespace App\Domain\Models;
+
+use App\Helpers\Core\PDOService;
+
+class ProductsModel extends BaseModel
+{
+    public function __construct(PDOService $pdo_service)
+    {
+        parent::__construct($pdo_service);
+    }
+
+    public function getAllProducts()
+    {
+        return $this->selectAll("SELECT * FROM PRODUCT");
+    }
+
+    public function getOneProduct($id)
+    {
+        $sql = "SELECT * FROM PRODUCT WHERE id = :id";
+
+        return $this->selectOne($sql, ['id' => $id]);
+    }
+
+    public function getProductByUPC($upc)
+    {
+        $sql = "SELECT * FROM PRODUCT WHERE upc = :upc";
+        return $this->selectOne($sql, ['upc' => $upc]);
+    }
+
+    public function getProductByEPC($epc)
+    {
+        $sql = "SELECT * FROM PRODUCT WHERE epc = :epc";
+        return $this->selectOne($sql, ['epc' => $epc]);
+    }
+
+    public function addProduct(array $data)
+    {
+        $this->execute(
+            'INSERT INTO PRODUCT (name, category, price, upc, epc, manufacturer, image_url, shelf_life_days)
+             VALUES (:name, :category, :price, :upc, :epc, :manufacturer, :image_url, :shelf_life_days)',
+            [
+                'name'           => $data['name'],
+                'category'       => $data['category'] ?? null,
+                'price'          => $data['price'],
+                'upc'            => $data['upc'] ?? null,
+                'epc'            => $data['epc'] ?? null,
+                'manufacturer'   => $data['manufacturer'] ?? null,
+                'image_url'      => $data['image_url'] ?? null,
+                'shelf_life_days'=> $data['shelf_life_days'] ?? null,
+            ]
+        );
+
+        return $this->lastInsertId();
+    }
+
+    public function updateProduct($id, array $data)
+    {
+        return $this->execute(
+            'UPDATE PRODUCT SET name = :name, category = :category, price = :price,
+             upc = :upc, epc = :epc, manufacturer = :manufacturer,
+             image_url = :image_url, shelf_life_days = :shelf_life_days WHERE id = :id',
+            [
+                'id'             => $id,
+                'name'           => $data['name'],
+                'category'       => $data['category'] ?? null,
+                'price'          => $data['price'],
+                'upc'            => $data['upc'] ?? null,
+                'epc'            => $data['epc'] ?? null,
+                'manufacturer'   => $data['manufacturer'] ?? null,
+                'image_url'      => $data['image_url'] ?? null,
+                'shelf_life_days'=> $data['shelf_life_days'] ?? null,
+            ]
+        );
+    }
+
+    public function deleteProduct($id)
+    {
+        return $this->execute("DELETE FROM PRODUCT WHERE id = :id", ['id' => $id]);
+    }
+
+    public function getAllStock()
+    {
+        return $this->selectAll(
+            "SELECT p.id, p.name, p.category, p.price,
+                    COALESCE(sr.current_stock, 0) AS current_stock, sr.date_received
+             FROM PRODUCT p
+             LEFT JOIN STOCK_RECEPTION sr ON sr.product_id = p.id
+             ORDER BY p.id, sr.date_received DESC"
+        );
+    }
+
+    public function getStockByProduct($product_id)
+    {
+        $sql = "SELECT * FROM STOCK_RECEPTION WHERE product_id = :product_id ORDER BY date_received DESC";
+        return $this->selectAll(
+            $sql,
+            ['product_id' => $product_id]
+        );
+    }
+
+    public function receiveStock(array $data)
+    {
+        $this->execute(
+            'INSERT INTO STOCK_RECEPTION (product_id, quantity_received, date_received, current_stock)
+             VALUES (:product_id, :quantity_received, :date_received, :current_stock)',
+            [
+                'product_id'        => $data['product_id'],
+                'quantity_received' => $data['quantity_received'],
+                'date_received'     => $data['date_received'],
+                'current_stock'     => $data['current_stock'],
+            ]
+        );
+
+        return $this->lastInsertId();
+    }
+
+    public function updateStock($product_id, int $new_stock)
+    {
+        $sql = 'UPDATE STOCK_RECEPTION SET current_stock = :current_stock
+                WHERE product_id = :product_id ORDER BY date_received DESC LIMIT 1';
+        return $this->execute($sql, ['product_id' => $product_id, 'current_stock' => $new_stock]);
+    }
+}
