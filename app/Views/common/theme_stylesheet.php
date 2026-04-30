c6f23caf1318811f2fa46f973197567589396702
Russel Canlapan <russeljcanlapan@gmail.com>
2026-04-29 23:29:07 -0400

    fixing admin header

--- app/Views/common/theme_stylesheet.php
+++ app/Views/common/theme_stylesheet.php
@@ -7,4 +7,5 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Jaldi:wght@400;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="<?= hs(public_asset_href('css/layout/customer.css')) ?>">
 <link rel="stylesheet" href="<?= hs(public_asset_href('css/theme.css')) ?>">
